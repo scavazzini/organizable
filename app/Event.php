67278e442ce8e30e38a33cc3318213ef719c53b9
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-12 00:38:07 -0300

    Create many-to-many relationship between Event and User

--- app/Event.php
+++ app/Event.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = ['title', 'description', 'start_at', 'end_at'];
+
+    public function participants()
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('owner')
+            ->withTimestamps();
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function events()
+    {
+        return $this->belongsToMany(Event::class)
+            ->withPivot('owner')
+            ->withTimestamps();
+    }
 }
