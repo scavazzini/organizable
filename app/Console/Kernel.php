e1efc436cee4b0809cbfdd90631d8b841ecc399c
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-14 04:11:26 -0300

    Schedule upcoming events email notification daily at midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\NotifyUsersWithUpcomingEvents;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new NotifyUsersWithUpcomingEvents(3))
+            ->dailyAt('00:00');
     }
 
     /**
