b3547e5fef08e96be6881e00549c71cc82fc8e36
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-22 00:26:48 -0300

    Add eloquent properties in NotificationType model

--- app/NotificationType.php
+++ app/NotificationType.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationType extends Model
 {
+    protected $fillable = ['id', 'name', 'description'];
+
+    public $timestamps = false;
+    public $incrementing = false;
+
     public function users()
     {
         return $this->belongsToMany(User::class)
