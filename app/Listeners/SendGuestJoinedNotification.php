be7e46e1812cceaae9acb2ddca0fca42dcaffecd
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-23 10:35:31 -0300

    Only send guest-joined email if user is notifiable by this type

--- app/Listeners/SendGuestJoinedNotification.php
+++ app/Listeners/SendGuestJoinedNotification.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use App\Events\GuestJoined;
 use App\Mail\GuestJoinedMail;
+use App\NotificationType;
+use App\Repositories\UserRepositoryInterface;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
@@ -17,7 +19,16 @@ class SendGuestJoinedNotification implements ShouldQueue
 
    public function handle(GuestJoined $guestJoined)
     {
-        $mail = new GuestJoinedMail($guestJoined->guest, $guestJoined->event);
-        Mail::to($guestJoined->owner->email)->queue($mail);
+        $owner = $guestJoined->owner;
+        $guest = $guestJoined->guest;
+        $event = $guestJoined->event;
+        $userRepository = app()->make(UserRepositoryInterface::class);
+
+        $isNotifiable = $userRepository->isNotifiableBy($owner, NotificationType::GUEST_JOINED);
+
+        if ($isNotifiable) {
+            $mail = new GuestJoinedMail($guest, $event);
+            Mail::to($owner->email)->queue($mail);
+        }
     }
 }
--- app/NotificationType.php
+++ app/NotificationType.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationType extends Model
 {
+    public const UPCOMING_EVENTS = 'upcoming-events';
+    public const GUEST_JOINED = 'guest-joined';
+
     protected $fillable = ['id', 'name', 'description'];
 
     public $timestamps = false;
