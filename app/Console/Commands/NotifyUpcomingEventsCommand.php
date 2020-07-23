37095309b49e09c6a7816102315e758f21604ae0
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-23 00:18:50 -0300

    Add callbacks and progress bar on users:notify-upcoming command

--- app/Console/Commands/NotifyUpcomingEventsCommand.php
+++ app/Console/Commands/NotifyUpcomingEventsCommand.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 
 class NotifyUpcomingEventsCommand extends Command
 {
-    protected $signature = 'users:notify-upcoming';
+    protected $signature = 'users:notify-upcoming {--queue : Queue this action}';
     protected $description = 'Notify users with upcoming events by email';
 
     public function __construct()
@@ -24,7 +24,32 @@ class NotifyUpcomingEventsCommand extends Command
             return 1;
         }
 
-        NotifyUsersWithUpcomingEvents::dispatch(3);
+        $notifyJob = new NotifyUsersWithUpcomingEvents(3);
+
+        if ($this->option('queue')) {
+            dispatch($notifyJob);
+            return 0;
+        }
+
+        $bar = $this->output->createProgressBar();
+
+        $notifyJob->setOnStart(function($users) use ($bar) {
+            $this->line(" Queuing emails...");
+            $bar->setMaxSteps($users);
+            $bar->start();
+        });
+
+        $notifyJob->setOnUpdate(function () use ($bar) {
+            $bar->advance();
+        });
+
+        $notifyJob->setOnFinish(function () use ($bar) {
+            $bar->finish();
+            $this->line(' Done.' . PHP_EOL);
+        });
+
+        dispatch_now($notifyJob);
+
         return 0;
     }
 }
--- app/Jobs/NotifyUsersWithUpcomingEvents.php
+++ app/Jobs/NotifyUsersWithUpcomingEvents.php
@@ -13,9 +13,15 @@ use Illuminate\Support\Facades\Mail;
 
 class NotifyUsersWithUpcomingEvents implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable;
+    use SerializesModels {
+        __serialize as traitSerialize;
+    }
 
     private $days;
+    private $onStartCallback;
+    private $onUpdateCallback;
+    private $onFinishCallback;
 
     public function __construct(int $days = 3)
     {
@@ -26,8 +32,59 @@ class NotifyUsersWithUpcomingEvents implements ShouldQueue
     {
         $users = $userRepository->getUsersWithUpcomingEvents($this->days);
 
+        $this->onStart(count($users));
+
         foreach ($users as $user) {
             Mail::to($user->email)->queue(new UpcomingEventsMail($user, $user->events->all()));
+            $this->onUpdate();
         }
+
+        $this->onFinish();
+    }
+
+    public function setOnStart(callable $onStartCallback)
+    {
+        $this->onStartCallback = $onStartCallback;
+    }
+
+    public function setOnUpdate(callable $onUpdateCallback)
+    {
+        $this->onUpdateCallback = $onUpdateCallback;
+    }
+
+    public function setOnFinish(callable $onFinishCallback)
+    {
+        $this->onFinishCallback = $onFinishCallback;
+    }
+
+    private function onStart(int $count)
+    {
+        if (is_callable($this->onStartCallback)) {
+            ($this->onStartCallback)($count);
+        }
+    }
+
+    private function onUpdate()
+    {
+        if (is_callable($this->onUpdateCallback)) {
+            ($this->onUpdateCallback)();
+        }
+    }
+
+    private function onFinish()
+    {
+        if (is_callable($this->onFinishCallback)) {
+            ($this->onFinishCallback)();
+        }
+    }
+
+    public function __serialize()
+    {
+        // Remove callbacks before serialization
+        $this->onStartCallback = null;
+        $this->onUpdateCallback = null;
+        $this->onFinishCallback = null;
+
+        return $this->traitSerialize();
     }
 }
