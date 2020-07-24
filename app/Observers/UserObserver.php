e0a91e23113957a2e65e3ea299b7e64443b531ea
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-07-24 14:31:59 -0300

    Use UserRepository in User Observer

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,15 +3,22 @@
 namespace App\Observers;
 
 use App\NotificationType;
+use App\Repositories\UserRepositoryInterface;
 use App\User;
 
 class UserObserver
 {
+    private $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+
     public function created(User $user)
     {
-        $user->notification_types()->attach([
-            NotificationType::UPCOMING_EVENTS,
-            NotificationType::GUEST_JOINED,
-        ]);
+        $this->userRepository->addNotification($user, NotificationType::UPCOMING_EVENTS);
+        $this->userRepository->addNotification($user, NotificationType::GUEST_JOINED);
     }
 }
